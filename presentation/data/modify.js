1ec91d726200be580860d1fce96b25aa5322323a
joelcororonah <joelcoronah@gmail.com>
2018-06-26 01:06:48 -0400

    cambios boton retroceder

--- presentation/data/modify.js
+++ presentation/data/modify.js
@@ -1,19 +1,19 @@
+var ispringPresentationConnector = {};
+       ispringPresentationConnector.register = function(player)
+        {             
+            function receiveMessage(event)
+            {
+            var playercito =player.presentation().slides().convertTimestampToTime(player.view().playbackController().clock().timestamp(),true,true)
+            rollbackTenSeconds(playercito,player)
+			}
+			window.addEventListener("message", receiveMessage, false);    
 
-var rollBackTenSec;
-console.log('hola')
-document.querySelector('.universal_mini > .bottom_panel').appendChild(document.createElement("div")).id='undoDiv_id'	
-document.getElementById('undoDiv_id').appendChild(document.createElement("div")).id='labelUndoDiv_id'
-document.getElementById('labelUndoDiv_id').className="botonRetrocede"
-var labelid = document.getElementById('labelUndoDiv_id')
-console.log(labelid)
-		var ispringPresentationConnector = {}
-		ispringPresentationConnector.register = function(player)
-		{
-			rollBackTenSec=player.view().playbackController()
-			console.log(rollBackTenSec)
-			labelid.onclick=function() {
-				rollBackTenSec.gotoPreviousSlide();
-				console.log('c')
-				}
-			
-		 }
+        };
+        function rollbackTenSeconds(timeNow,playerx){
+                if(timeNow>10){
+                    var timecito=playerx.presentation().slides().convertTimeToTimestamp(timeNow-10,true)
+                    var goBackten=playerx.view().playbackController().gotoTimestamp(timecito.Nb,timecito.Jr,timecito.gh,true)
+                }else{
+                    playerx.view().playbackController().gotoFirstSlide(true)
+                }
+        }  
