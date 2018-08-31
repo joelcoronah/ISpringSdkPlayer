fa8bf2269b99d8f2bd003466d6f7c689cf15d2b2
joelcoronah <joelcoronah@gmail.com>
2018-08-30 21:49:37 -0400

    changes in background color

--- res/data/modify.js
+++ res/data/modify.js
@@ -18,7 +18,6 @@ var ispringPresentationConnector = {};
         };
         function rollbackTenSeconds(timeNow,playerx){  
             var timecito=playerx.presentation().slides().convertTimeToTimestamp(Math.max(0,timeNow-10),true)
-            //var goBackten=playerx.view().playbackController().gotoTimestamp(timecito.Nb,timecito.Jr,timecito.gh,false)
             playerx.view().playbackController().gotoTimestamp(timecito.slideIndex(),timecito.stepIndex(),timecito.timeOffset(),true);
 
         }          
